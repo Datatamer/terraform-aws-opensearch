be79f0dc6cc3df836c9c9d6985c03aacf346a2ce
Hugo Barros <hugo.barros@caylent.com>
2021-08-31 10:18:39 -0300

    big refactor. test_structure + var map

--- test/create_es_test.go
+++ test/create_es_test.go
@@ -1,48 +1,100 @@
 package test
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/aws"
+	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
-	"github.com/stretchr/testify/assert"
+	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
+	"github.com/stretchr/testify/require"
 )
 
-func TestTerraformCreateES(t *testing.T) {
+func TestMinimalElasticSearch(t *testing.T) {
 	// This creates this function with the possibility of being run in parallel with other functions
 	// that start with this same t.Parallel() call.
 	t.Parallel()
 
-	namePrefix := "terratest-es"
+	// For convenience - uncomment these as well as the "os" import
+	// when doing local testing if you need to skip any sections.
+	//
+	// A common usage for this would be skipping teardown first (keeping infrastructure)
+	// and then in your next run skip the setup* and create* steps. This way you can keep testing
+	// your Go test against your infrastructure quicker. Be mindful of random-ids, as they would be updated
+	// on each run, which would make some assertions fail.
+
+	// os.Setenv("SKIP_", "true")
+	// os.Setenv("TERRATEST_REGION", "us-east-1")
+
+	// os.Setenv("SKIP_setup_options", "true")
+	// os.Setenv("SKIP_create_cluster", "true")
+	// os.Setenv("SKIP_validate", "true")
+
+	// os.Setenv("SKIP_teardown", "true")
+
+	// list of different buckets that will be created to be tested
+	var testCases = []ElasticSearchModuleTestCase{
+		{
+			testName:         "Test1",
+			expectApplyError: false,
+			vars: map[string]interface{}{
+				"name-prefix":             strings.ToLower(random.UniqueId()),
+				"tags":                    make(map[string]string),
+				"create_new_service_role": false,
+			},
+		},
+	}
 
 	// Getting a random region between the US ones
 	awsRegion := aws.GetRandomRegion(t, []string{"us-east-1", "us-east-2", "us-west-1", "us-west-2"}, nil)
 
-	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-		// The path to where our Terraform code is located
-		TerraformDir: "../test_examples/minimal",
+	for _, testCase := range testCases {
+		testCase := testCase
 
-		// Test Root Module Variables
-		Vars: map[string]interface{}{
-			"name-prefix": namePrefix,
-		},
-		// Environment variables to set when running Terraform
-		EnvVars: map[string]string{
-			"AWS_DEFAULT_REGION": awsRegion,
-		},
-	})
+		t.Run(testCase.testName, func(t *testing.T) {
+			t.Parallel()
 
-	// Run destroy when this function exits
-	defer terraform.Destroy(t, terraformOptions)
-	// Run TF Init and Apply in the TerraformDir passed in options
-	terraform.InitAndApply(t, terraformOptions)
+			// this creates a tempTestFolder for each testCase
+			tempTestFolder := test_structure.CopyTerraformFolderToTemp(t, "..", "test_examples/minimal")
 
-	es_id := terraform.Output(t, terraformOptions, "tamr_es_domain_id")
-	es_endp := terraform.Output(t, terraformOptions, "tamr_es_domain_endpoint")
-	es_sg_ids := terraform.OutputList(t, terraformOptions, "es_security_group_ids")
+			test_structure.RunTestStage(t, "setup_options", func() {
+				terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+					TerraformDir: tempTestFolder,
+					Vars:         testCase.vars,
+					EnvVars: map[string]string{
+						"AWS_REGION": awsRegion,
+					},
+				})
 
-	assert.NotNil(t, es_id)
-	assert.NotNil(t, es_endp)
-	assert.NotNil(t, es_sg_ids)
+				test_structure.SaveTerraformOptions(t, tempTestFolder, terraformOptions)
+			})
+
+			test_structure.RunTestStage(t, "create_cluster", func() {
+				terraformOptions := test_structure.LoadTerraformOptions(t, tempTestFolder)
+
+				_, err := terraform.InitAndApplyE(t, terraformOptions)
+
+				if testCase.expectApplyError {
+					require.Error(t, err)
+					// If it failed as expected, we should skip the rest (validate function).
+					t.SkipNow()
+				}
+			})
+
+			defer test_structure.RunTestStage(t, "teardown", func() {
+				teraformOptions := test_structure.LoadTerraformOptions(t, tempTestFolder)
+				terraform.Destroy(t, teraformOptions)
+			})
+
+			test_structure.RunTestStage(t, "validate", func() {
+				terraformOptions := test_structure.LoadTerraformOptions(t, tempTestFolder)
+				validateModuleOutputs(t,
+					terraformOptions,
+				)
+			})
+
+		})
+	}
 
 }
