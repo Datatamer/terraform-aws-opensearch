76c072154130a9695eb4fc18270b24ba6adcbd7b
Hugo Barros <hugo.barros@caylent.com>
2022-02-02 15:47:04 -0300

    adding example for logs

---
+++ examples/logs/main.tf
@@ -0,0 +1,54 @@
+resource "aws_vpc" "es_vpc" {
+  cidr_block = "1.2.3.0/24"
+  tags       = var.tags
+}
+
+resource "aws_subnet" "es_subnet" {
+  vpc_id     = aws_vpc.es_vpc.id
+  cidr_block = "1.2.3.0/24"
+  tags       = var.tags
+}
+
+module "sg-ports" {
+  #source = "git::https://github.com/Datatamer/terraform-aws-es.git//modules/es-ports?ref=2.0.0"
+  source = "../../modules/es-ports"
+}
+
+module "aws-sg" {
+  source = "git::git@github.com:Datatamer/terraform-aws-security-groups.git?ref=0.1.0"
+  vpc_id = aws_vpc.es_vpc.id
+  ingress_cidr_blocks = [
+    "1.2.3.0/24"
+  ]
+  egress_cidr_blocks = [
+    "0.0.0.0/0"
+  ]
+  ingress_ports  = module.sg-ports.ingress_ports
+  sg_name_prefix = var.name-prefix
+  tags           = var.tags
+}
+
+resource "aws_iam_service_linked_role" "es" {
+  count            = var.create_new_service_role == true ? 1 : 0
+  aws_service_name = "es.amazonaws.com"
+}
+
+module "tamr-es-cluster" {
+  depends_on         = [aws_iam_service_linked_role.es]
+  source             = "../../"
+  vpc_id             = aws_vpc.es_vpc.id
+  domain_name        = format("%s-elasticsearch", var.name-prefix)
+  subnet_ids         = [aws_subnet.es_subnet.id]
+  security_group_ids = module.aws-sg.security_group_ids
+  log_types          = var.log_types
+  log_group_name     = aws_cloudwatch_log_group.es-logs.name
+  tags               = var.tags
+}
+
+#tfsec:ignore:aws-cloudwatch-log-group-customer-key
+resource "aws_cloudwatch_log_group" "es-logs" {
+  name_prefix = format("%s-%s", var.name-prefix, "example-logs")
+
+  retention_in_days = var.log_retention_in_days
+  tags              = var.tags
+}
---
+++ examples/logs/outputs.tf
@@ -0,0 +1,22 @@
+output "tamr_es_domain_id" {
+  value       = module.tamr-es-cluster.tamr_es_domain_id
+  description = "ID of the security group created"
+}
+
+output "tamr_es_domain_endpoint" {
+  value       = module.tamr-es-cluster.tamr_es_domain_endpoint
+  description = "Endpoint of ES domain created"
+}
+
+output "es_security_group_id" {
+  value       = module.tamr-es-cluster.es_security_group_ids
+  description = "ID of the ES domain created"
+}
+
+output "vpc_id" {
+  value = aws_vpc.es_vpc.id
+}
+
+output "subnet_id" {
+  value = aws_subnet.es_subnet.id
+}
